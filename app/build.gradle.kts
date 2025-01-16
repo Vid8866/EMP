2b94056db272b61ccefe0163b0bc4a1480cfa32b
Vid Grozdanov <vid.grozdanov@gmail.com>
2025-01-16 14:01:39 +0100

    Login working

---
+++ app/build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+}
+
+android {
+    namespace = "com.example.shred"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.shred"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0")) // Check for the latest version
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    implementation(libs.firebase.database)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+apply(plugin = "com.google.gms.google-services")
---
+++ app/src/main/java/com/example/shred/LoginScreen.kt
@@ -0,0 +1,74 @@
+package com.example.shred
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.shred.databinding.ActivityLoginScreenBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.InternalHelpers
+import com.google.firebase.database.ValueEventListener
+
+class LoginScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginScreenBinding
+    private lateinit var firebaseDatabase: FirebaseDatabase
+    private lateinit var databaseReference: DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.reference.child("users")
+
+        binding.loginButton.setOnClickListener {
+            val loginUsername = binding.loginUsername.text.toString()
+            val loginPassword = binding.loginPassword.text.toString()
+
+            if(loginPassword.isNotEmpty() && loginPassword.isNotEmpty()){
+                loginUser(loginUsername, loginPassword)
+            } else {
+                Toast.makeText(this@LoginScreen, "Please fill all the fields", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.signupRedirect.setOnClickListener {
+            startActivity(Intent(this@LoginScreen, RegistrationScreen::class.java))
+            finish()
+        }
+    }
+
+
+    private fun loginUser(username: String, password: String){
+        databaseReference.orderByChild("username").equalTo(username).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if (dataSnapshot.exists()){
+                    for (userSnapshot in dataSnapshot.children){
+                        val userData = userSnapshot.getValue(UserData::class.java)
+
+                        if (userData != null && userData.password == password){
+                            if (userData.password == password){
+                                Toast.makeText(this@LoginScreen, "Login successful", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this@LoginScreen, MainActivity::class.java))
+                                finish()
+                                return
+                            }
+                        }
+                    }
+                    Toast.makeText(this@LoginScreen, "Login failed", Toast.LENGTH_SHORT).show()
+                }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Toast.makeText(this@LoginScreen, "Database Error: ${databaseError.message}", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+}
